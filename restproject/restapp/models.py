23db29d761c1e32de2260deb7c5747e35f551936
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-07 16:52:56 +0900

    update page 연결 및 추가 업데이트

--- restproject/restapp/models.py
+++ restproject/restapp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Music(models.Model):
     title = models.CharField(max_length = 50)
     singer = models.ForeignKey('Singer', on_delete = models.CASCADE)
-    genres = models.ForeignKey('Genre', on_delete = models.CASCADE)
+    genre = models.ForeignKey('Genre', on_delete = models.CASCADE)
     released_at = models.DateField()
     def __str__(self):
         return self.title
--- restproject/restapp/urls.py
+++ restproject/restapp/urls.py
@@ -5,4 +5,6 @@ from restapp import views
 urlpatterns = [
     path('', views.index_view, name = 'index'),
     path('<int:pk>/', views.detail_view, name = 'detail'),
+    path('new/', views.create_new, name = 'create'),
+    path('<int:pk>/update/', views.update_view, name = 'update'),
 ]
